0b2d7e255be59c8b8518ee3f75417f37f45ba73b
andrea-delaflor <andrea.delaflor@alumnos.upm.es>
2026-04-08 15:40:18 +0200

    clase pieza

--- src/Pieza.cpp
+++ src/Pieza.cpp
@@ -1,9 +1,1 @@
-#include <iostream>
-// #include "Pieza.h" 
-
-// Constructor de la clase Pieza
- /*Pieza::Pieza(std::string nombre, std::string color) {
-	this->nombre = nombre;
-	this->color = color;
-}
-*/
+#include "Pieza.h"
