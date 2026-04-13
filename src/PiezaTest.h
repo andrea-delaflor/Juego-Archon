db696244de2e526701fdb014106dd55b9b9d090b
Carlota-pg <carlota.pinedo03@gmail.com>
2026-04-13 13:36:37 +0200

    Pieza actualizada y movimiento

--- src/PiezaTest.h
+++ src/PiezaTest.h
@@ -4,23 +4,21 @@
 
 class PiezaTest : public Pieza {
 public:
+    
     PiezaTest(Vector2D pos, Bando b) :
         Pieza("Test", b, 100, 1.0f, 10, 1.0f, 1.0f, 3, TipoArma::MAGIA, pos) {
     }
 
-    std::vector<Vector2D> obtenerMovimientosValidos(Tablero* t) override { return {}; }
+    // Especificamos el tipo de pieza
     TipoMovimiento obtenerTipoMovimiento() override { return TipoMovimiento::TERRESTRE; }
+
     std::string obtenerNombreSprite() override { return ""; }
 
     void dibuja() override {
-        // Esta es la lógica que usabas para el punto rojo
         float x_gl = (float)posicion.x - 4.0f;
         float y_gl = 4.0f - (float)posicion.y;
-
-        // Dibujamos con el color del bando
-        if (bando == Bando::LUZ) glColor3f(0.0f, 1.0f, 1.0f); // Cyan
-        else glColor3f(1.0f, 0.0f, 1.0f);                    // Magenta
-
+        if (bando == Bando::LUZ) glColor3f(0.0f, 1.0f, 1.0f);
+        else glColor3f(1.0f, 0.0f, 1.0f);
         glBegin(GL_QUADS);
         glVertex2f(x_gl - 0.3f, y_gl - 0.3f);
         glVertex2f(x_gl + 0.3f, y_gl - 0.3f);
