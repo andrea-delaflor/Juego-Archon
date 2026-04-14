23ed355ae6c76ea34498cb603a445df7781b8a7c
juliamagallares <julia.magallares@alumnos.upm.es>
2026-04-14 12:20:20 +0200

    archivo cabezera djini

--- src/DjiniL.h
+++ src/DjiniL.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Pieza.h"
+#include "ETSIDI.h" 
+
+class DjiniL : public Pieza {
+public:
+    DjiniL(Vector2D posInicial);
+    ~DjiniL() override;
+
+    std::string obtenerNombreSprite() override;
+    void dibuja() override;
+    TipoMovimiento obtenerTipoMovimiento() override;
+
+private:
+    ETSIDI::Sprite* sprite;
+};
+
