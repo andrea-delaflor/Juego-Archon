c90b0e25f60aef88f452b5d2dfc84b9a04ec8fb7
juliamagallares <julia.magallares@alumnos.upm.es>
2026-04-14 12:18:07 +0200

    incluir archivo.h de dragon

--- src/DragonO.h
+++ src/DragonO.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "Pieza.h"
+#include "ETSIDI.h"
+
+class DragonO : public Pieza {
+public:
+    DragonO(Vector2D posInicial);
+    ~DragonO() override;
+
+    std::string obtenerNombreSprite() override;
+    void dibuja() override;
+    TipoMovimiento obtenerTipoMovimiento() override;
+
+private:
+    ETSIDI::Sprite* sprite;
+};
