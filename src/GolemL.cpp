e9ee72966a04fba9b555f7fa4e45a76e6b183e0a
juliamagallares <julia.magallares@alumnos.upm.es>
2026-04-14 12:35:25 +0200

    solucion problema ver en tablero

--- src/GolemL.cpp
+++ src/GolemL.cpp
@@ -1,11 +1,15 @@
-#include"GolemL.h"
+#include "GolemL.h"
+#include "glut.h" // Añadimos esto para poder usar los comandos de OpenGL
 
 void GolemL::dibuja() {
+    // 1. APAGAMOS LAS TEXTURAS TEMPORALMENTE PARA DIBUJAR FORMAS PURAS
+    glDisable(GL_TEXTURE_2D);
+
     //Convertir de coordenadas tablero a las de Glut
     float x_gl = (float)posicion.x - 4.0f;
     float y_gl = 4.0f - (float)posicion.y;
 
-    //color pieza
+    //color pieza (Cian)
     glColor3f(0.0f, 1.0f, 1.0f);
 
     //Forma Golem ---->Por ahora cuadrado
@@ -15,4 +19,7 @@ void GolemL::dibuja() {
     glVertex2f(x_gl + 0.35f, y_gl + 0.35f);
     glVertex2f(x_gl - 0.35f, y_gl + 0.35f);
     glEnd();
+
+    // 2. VOLVEMOS A ENCENDER LAS TEXTURAS PARA NO ROMPER LAS DEMÁS FICHAS
+    glEnable(GL_TEXTURE_2D);
 }
